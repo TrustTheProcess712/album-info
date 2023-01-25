d64346dcf1d1ff679e58492bc37a4d08eb4d13f0
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2023-01-25 14:15:50 +0000

    adds extra search features in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,24 +5,33 @@ const Home = () => {
   const [albumData, setAlbumData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
 
-  const handleSearch = (e) => {
-    const searchInput = e.target.value;
-    const searchResult = albumData.filter((search) => {
-      return search.title.toLowerCase().includes(searchInput.toLowerCase());
-    });
-    console.log(searchInput);
-    setFilteredData(searchResult);
-  };
-
   useEffect(() => {
     fetch("http://localhost:8000/albums")
       .then((res) => res.json())
       .then((albumsFromAPI) => setAlbumData(albumsFromAPI));
   }, []);
 
+  const handleSearch = (e) => {
+    const searchInput = e.target.value.toString();
+
+    const searchResult = albumData.filter((albums) => {
+      return (
+        albums.year.toString() === searchInput ||
+        albums.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+        albums.artist.toLowerCase().includes(searchInput.toLowerCase())
+      );
+    });
+
+    setFilteredData(searchResult);
+  };
+
   return (
     <div className='home'>
-      <input onChange={handleSearch} type='text' placeholder='Search' />
+      <input
+        onChange={handleSearch}
+        type='text'
+        placeholder='Search title, year, genre...'
+      />
       {filteredData.length === 0 ? (
         <AlbumList albumData={albumData} title='Preview' />
       ) : (
